cb8f7e1fff58ed9246b6c9a832a4ad49fbef085b
Lauri-Lam <laurilam360@gmail.com>
2024-05-16 17:53:29 +0300

    aamujahh

--- Views/CalendarPage.xaml.cs
+++ Views/CalendarPage.xaml.cs
@@ -1,9 +1,56 @@
-namespace MokinVarausApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Syncfusion.Maui.Calendar;
 
-public partial class CalendarPage : ContentPage
+namespace MokinVarausApp.Views
 {
-	public CalendarPage()
-	{
-		InitializeComponent();
-	}
+    public partial class CalendarPage : ContentPage
+    {
+        public CalendarPage()
+        {
+            InitializeComponent();
+            LoadCalendar();
+        }
+
+        private async void LoadCalendar()
+        {
+            var reservations = await GetReservationsAsync();
+            var appointments = new List<CalendarInlineEvent>();
+
+            foreach (var reservation in reservations)
+            {
+                for (DateTime date = reservation.ArrivalDate; date <= reservation.DepartureDate; date = date.AddDays(1))
+                {
+                    appointments.Add(new CalendarInlineEvent
+                    {
+                        StartTime = date,
+                        EndTime = date,
+                        Subject = "Reserved",
+                        Color = Colors.Red
+                    });
+                }
+            }
+
+            Calendar.DataSource = appointments;
+        }
+
+        private async Task<List<Reservation>> GetReservationsAsync()
+        {
+            // Replace with your actual database access code
+            await Task.Delay(500); // Simulate async delay
+            return new List<Reservation>
+            {
+                new Reservation { ArrivalDate = new DateTime(2024, 5, 1), DepartureDate = new DateTime(2024, 5, 10) }
+            };
+        }
+
+        public class Reservation
+        {
+            public DateTime ArrivalDate { get; set; }
+            public DateTime DepartureDate { get; set; }
+        }
+    }
 }
